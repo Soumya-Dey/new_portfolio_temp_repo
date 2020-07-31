bbfbcb509e3cabf9e28ac3289786607765cc60b9
Soumya Dey <soumyadey200@hotmail.com>
2020-07-31 20:14:36 +0530

    first comit

---
+++ src/components/Landing.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Fade, Zoom } from "@material-ui/core";
+
+export const Landing = () => {
+    return (
+        <div className="container">
+            <div className="right-clip-1"></div>
+            <div className="right-clip-2"></div>
+
+            <div className="flex-c-spbw">
+                <Zoom in={true} timeout={600}>
+                    <div className="logo">dEY.</div>
+                </Zoom>
+
+                <div>
+                    <div className="social-icons">
+                        <Fade in={true} timeout={1400}>
+                            <div className="h-line"></div>
+                        </Fade>
+
+                        <div className="icons">
+                            <Zoom in={true} timeout={600}>
+                                <a
+                                    href="https://www.instagram.com/soumya_sl/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                            </Zoom>
+                            <Zoom in={true} timeout={600}>
+                                <a
+                                    href="https://github.com/Soumya-Dey"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <i class="fab fa-github"></i>
+                                </a>
+                            </Zoom>
+                            <Zoom in={true} timeout={600}>
+                                <a
+                                    href="https://twitter.com/soumyadey2001"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </Zoom>
+                        </div>
+                    </div>
+
+                    <Fade in={true} timeout={1400}>
+                        <div className="text-cont">
+                            <a href="#!" className="vert-btn">
+                                view my work
+                            </a>
+
+                            <div className="landing-text">
+                                Hi there, I’m Soumya.
+                                <br />
+                                Full Stack <span>Developer</span>,<br />
+                                Student, and intern at Rent2Trust.
+                            </div>
+                        </div>
+                    </Fade>
+                </div>
+            </div>
+        </div>
+    );
+};
