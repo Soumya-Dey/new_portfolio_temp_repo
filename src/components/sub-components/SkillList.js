40b937ba70d84576881537afb041be308671cf5b
Soumya Dey <soumyadey200@hotmail.com>
2020-10-19 19:48:27 +0530

    added react native to skills

--- src/components/sub-components/SkillList.js
+++ src/components/sub-components/SkillList.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 export const SkillList = () => {
-    return (
-        <div className="skills-grid">
-            <li className="skill-item">HTML 5</li>
-            <li className="skill-item">CSS 3</li>
-            <li className="skill-item">Javascript ES6</li>
-            <li className="skill-item">React</li>
-            <li className="skill-item">Redux</li>
-            <li className="skill-item">Node.js</li>
-            <li className="skill-item">Express.js</li>
-            <li className="skill-item">MongoDB</li>
-        </div>
-    );
+  return (
+    <div className="skills-grid">
+      <li className="skill-item">HTML 5</li>
+      <li className="skill-item">CSS 3</li>
+      <li className="skill-item">Javascript ES6</li>
+      <li className="skill-item">React</li>
+      <li className="skill-item">Redux</li>
+      <li className="skill-item">Node.js</li>
+      <li className="skill-item">Express.js</li>
+      <li className="skill-item">MongoDB</li>
+      <li className="skill-item skill-item-learning">React Native</li>
+    </div>
+  );
 };
